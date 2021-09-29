efa803aaf0e4c48454e92ccb3e61489adae8bebb
Joshua George <horheyinc8@gmail.com>
2021-09-29 14:21:56 -0500

    all routes set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import logo from "../images/andLogo.jpeg";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -7,38 +8,52 @@ function Navbar() {
   const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
 
   return (
-    <Nav variant="pills" activeKey="1" onSelect={handleSelect}>
-      <Nav.Item>
-        <Nav.Link eventKey="1" href="#/home">
-          Home{" "}
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="2" title="Item">
-          About us!{" "}
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="3" disabled>
-          Contact us!{" "}
-        </Nav.Link>
-      </Nav.Item>
-      <NavDropdown title="Porducts" id="nav-dropdown">
-        <NavDropdown.Item eventKey="4.1">Womens</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.2">
-          Mens & Patriotic / Political
-        </NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Youth</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Holidays / Seasonal</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Students & Teachers</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Tumblers</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Specialty Items</NavDropdown.Item>
-        <NavDropdown.Item eventKey="4.3">Tutu's</NavDropdown.Item>
+    <div>
+      <Nav
+        className="navbar"
+        variant="pills"
+        activeKey="1"
+        onSelect={handleSelect}
+      >
+        <Nav.Item>
+          <NavLink exact className="nav-link" eventKey="1" to="/">
+            Home
+          </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink className="nav-link" eventKey="1" to="/about">
+            About
+          </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+          <NavLink className="nav-link" eventKey="1" to="/contact">
+            Contact
+          </NavLink>
+        </Nav.Item>
+        <NavDropdown title="Products" id="nav-dropdown">
+          <NavDropdown.Item eventKey="4.1">Womens</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.2">
+            Mens & Patriotic / Political
+          </NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.3">Youth</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.4">
+            Holidays / Seasonal
+          </NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.5">
+            Students & Teachers
+          </NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.6">Tumblers</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.7">Specialty Items</NavDropdown.Item>
+          <NavDropdown.Item eventKey="4.8">Tutu's</NavDropdown.Item>
 
-        <NavDropdown.Divider />
-        <NavDropdown.Item eventKey="4.4">Follow us!</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
+          <NavDropdown.Divider />
+          <NavDropdown.Item eventKey="4.4">Follow us!</NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Item>
+          <img className="nav-logo" src={logo} alt="logo" />
+        </Nav.Item>
+      </Nav>
+    </div>
   );
 
   // <div className="navbar">
