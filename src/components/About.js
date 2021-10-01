e976ea5bcbcf0f9b34fd81497d681795a56bb345
Joshua George <horheyinc8@gmail.com>
2021-09-30 21:22:48 -0500

    footer updated, but not finished

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,25 @@
 import React from "react";
+import familyPic from "../images/AndFamily.jpeg";
 
 function About() {
   return (
-    <div>
-      <h1>About page</h1>
+    <div className="about-page">
+      <h1>Hey guys I'm Ashley!</h1>
+
+      <img className="about-img" src={familyPic} alt="family" />
+      <p>
+        {" "}
+        I was born and raised here in Houston, Texas. I am the oldest of three
+        and some may say I'm a little bossy and by that I mean my siblings. Fun
+        fact about me, I am also a Christmas baby!!! Yay for my mom, right?
+      </p>
+      <p>
+        I am first a wife and mother... teenage boy mom to be exact. My small
+        business started as a hobby making tutu's for my friends little girls
+        and has expanded to add so many other things. I am truly grateful to all
+        my clients and customers because without them, this side hustle of mine
+        would never have been possible. I do hope to one day open up a store.
+      </p>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,30 @@
 import React from "react";
+import logo from "../images/andLogo.jpeg";
+import { SocialIcon } from "react-social-icons";
 
 function Footer() {
   return (
     <div className="footer-container">
-      <h1>this is the footer</h1>
+      <div className="footer-icons">
+        <SocialIcon
+          url="https://www.instagram.com/anddesigns.20/"
+          target="_blank"
+          fgColor="#fff"
+          style={{ height: 80, width: 80 }}
+        />
+        <SocialIcon
+          url="https://www.facebook.com/groups/anddesignsauctionsales"
+          target="_blank"
+          fgColor="#fff"
+          style={{ height: 80, width: 80 }}
+        />
+      </div>
+      <div>
+        <h3>Family owned and operated</h3>
+        <img className="footer-logo" src={logo} alt="logo" />
+      </div>
+
+      <div></div>
     </div>
   );
 }
